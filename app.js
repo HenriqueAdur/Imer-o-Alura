4dce6886e3df4e87f88eae26f332cac458907be4
HenriqueAdur <42901707+HenriqueAdur@users.noreply.github.com>
2024-09-06 13:51:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+function pesquisar() {
+    // console.log(dados);
+    let section = document.getElementById("resultados-pesquisa")
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Digite o nome de um artista ou banda</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultado = "";
+    let titulo = "";
+    let descricao = "";
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        if (titulo.includes(campoPesquisa)) {
+            resultado += `
+            <div class="item-resultado">
+                            <h2>
+                            <a href="" target="_blank">${dado.titulo}</a>
+                            </h2>
+                            <p class="descricao-meta"> RESUMO : ${dado.descricao}</p>
+                            <p class="descricao-meta">Genero : ${dado.genero}</p>
+                            <p class="descricao-meta">ACORDES : ${dado.acordes}</p>
+                             <p class="descricao-meta">SEQUENCIA ACORDES : ${dado.sequencia}</p>
+                            <A href="${dado.linkm}" target="_blank">Link Musica</A>
+                             <p class="descricao-meta"></p>
+                            <A href="${dado.linkc}" target="_blank">Link Cifra</A>
+                        </div>
+            `
+        }
+        if (!resultados) {
+            resultados = "<p>Nada foi encontrado</p>"
+
+        }
+        section.innerHTML = resultado
+    }
+}
---
+++ dados.js
@@ -0,0 +1,43 @@
+let dados = [ 
+    {
+    titulo: "America - A HORSE WITH NO NAME",
+    descricao: "America é uma banda anglo-americana de folk rock formada em Londres no ano de 1970 por Dewey Bunnell, Dan Peek e Gerry Beckley. Os músicos se conheceram por serem filhos de membros da Força Aérea Estadunidense, que estavam estacionados em Londres",
+    acordes: "Em = Mi menor / A9 = Lá com nona",
+    sequencia: " |: Em  | A9  | Em  | A9 :|",
+    genero: "Rock",
+    linkm: "https://www.youtube.com/watch?v=na47wMFfQCo",
+    linkc: "https://www.bananacifras.com/cifra/a/america/a-horse-with-no-name#splitTab",    
+    },
+    {
+        titulo: "Paralamas - La Bella Luna",
+        descricao: "",
+        acordes: "",
+        sequencia: "",
+    },
+];
+
+/* Musicas a serem adicionadas
+
+"Paralamas do Sucesso",
+"Guilherme Medeiros",
+"Skank",
+"Titãs",
+"Erasmo Carlos",
+"Arnaldo Antunes",
+"Bobby MC Ferrin",
+"John Lennon",
+"K'Naan",
+"Jota Quest",
+"Pearl Jam",
+"Chris Isaak",
+"Paul MC Cartney",
+"Legião Urbana",
+"Guns N' Roses",
+"Claudinho & Buchecha",
+"Cássia Eller",
+"Scott McKenzie",
+"R.E.M.",
+"Queen",
+"Zombies",
+"Adele",
+"Easy" */
